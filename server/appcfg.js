e771560603549d63cd707928ba1d3a243ffd5b3c
BolNick <BolNick09RUS@gmail.com>
2025-07-01 19:56:35 +0300

    + Сервер, неудачное подключение к СУБД

---
+++ server/appcfg.js
@@ -0,0 +1,27 @@
+module.exports = 
+{
+    Project_Root: 'C:\\Programs\\nvm\\Learning\\BlindMatching\\dist',
+
+    msconfig: 
+    {  
+        server: 'DESKTOP-ET8RJ3S',  //update me
+        authentication: 
+        {
+            type: 'default',
+            options: 
+            {
+                userName: 'DESKTOP-ET8RJ3S\\BolNi', //update me
+                password: ''  //update me
+            }
+        },
+        options: 
+        {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: false,
+            database: 'BlindMatching'  //update me
+        }
+    },
+    webPort: 3000 
+};
+
+ 
